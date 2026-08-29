2511e75b2f1019740aa15f7b77406f3ccfafc1fd
RehafAlatawi <136210563+RehafAlatawi@users.noreply.github.com>
2026-08-29 14:11:47 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,16 +1,14 @@
 from collections import Counter
-
 log_file = "sample_logs.txt"
-
 failed_ips = []
 
 with open(log_file, "r") as file:
-for line in file:
-if "Failed password" in line:
-ip = line.split("from ")[1].strip()
-failed_ips.append(ip)
-
+  for line in file:
+    if "Failed password" in line:
+      ip = line.split("from ")[1].strip()
+      failed_ips.append(ip)
+      
 ip_counts = Counter(failed_ips)
 
 for ip, count in ip_counts.items():
-print(f"{ip}: {count} failed login attempts")
+  print(f"{ip}: {count} failed login attempts")
